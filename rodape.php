81040e4639403e2fb30fec29481cac875c009d41
Manuella Maia Lopes <manuellamaiamana07@gmail.com>
2024-12-16 09:26:42 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <p>&copy; 2024 Lendas Urbanas. Todos os direitos reservados.</p>
+        <ul class="social">
+            <li><a href="http://facebook.com/mirrorfashion" title="Facebook">Facebook</a></li>
+            <li><a href="http://twitter.com/mirrorfashion" title="Twitter">Twitter</a></li>
+            <li><a href="http://instagram.com/mirrorfashion" title="Instagram">Instagram</a></li>
+        </ul>
+    </div>
+</footer>
